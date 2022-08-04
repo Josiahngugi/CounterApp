8c0c5e5743ee94ac1ab27134b1879eaa45363a66
Josiah Maina <41941177+Josiahngugi@users.noreply.github.com>
2022-08-04 00:26:09 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import "./styles.css";
+import React, { useState } from "react";
+
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  function addCount() {
+    setCount((prevValue) => {
+      return prevValue + 1;
+    });
+  }
+  function minusCount() {
+    setCount((prevValue) => {
+      return prevValue - 1;
+    });
+  }
+
+  return (
+    <div className="App">
+      <h1>{count}</h1>
+      <button onClick={addCount}>Increment</button>
+      <button onClick={minusCount}>Decrement</button>
+    </div>
+  );
+}
